b6539ff81f0d5f5b819f3882ddbc6cf467e9a5ad
chenbaof <198921646@163.com>
2015-10-22 09:58:02 +0800

    commit code

---
+++ main.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+var RaisedButton = require('material-ui/lib/raised-button');
+
+
+ReactDom.render(
+  <div>
+    <RaisedButton label="Default" />
+  </div>,
+  document.getElementById('example')
+);
